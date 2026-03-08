a45ff15e63b34875d2e0e8a5b50e7fa34efdfa53
manuel08hdz04-commits <manuel08hdz04@gmail.com>
2026-03-07 22:07:45 -0600

    Add files via upload

---
+++ Funcion_print().py
@@ -0,0 +1,12 @@
+print("Hola, mundo")
+print("Hernandez Ortega Victor Manuel")
+print('Hernandez Ortega Victor Manuel')
+
+#Errores de sintaxis
+#print(Hernandez Ortega Victor Manuel)
+#print"Hernandez Ortega Victor Manuel"
+
+#Palabras Clave
+print("Mi nombre es","\nVictor.", end=" ")
+print("Mi", "nombre", "es", sep="_",end="*")
+
