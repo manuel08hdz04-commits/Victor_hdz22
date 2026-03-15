d179ad2b426ad5c70af3253e305e6305350de8b9
manuel08hdz04-commits <manuel08hdz04@gmail.com>
2026-03-15 13:01:52 -0600

    Add files via upload

---
+++ Cómo interactúa la función con sus argumentos.py
@@ -0,0 +1,29 @@
+#Funciones y alcance la palabra clave global
+global name #funciòn que obliga a python a abstenerse de crear una nueva variable dentro de la función
+
+#Cómo interactúa la función con sus argumentos
+def my_function(my_list_1):
+    print("Print #1:", my_list_1)
+    print("Print #2:", my_list_2)
+    my_list_1 = [0, 1]
+    print("Print #3:", my_list_1)
+    print("Print #4:", my_list_2)
+
+
+my_list_2 = [2, 3]
+my_function(my_list_2)
+print("Print #5:", my_list_2)
+
+
+def my_function(my_list_1):
+    print("Print #1:", my_list_1)
+    print("Print #2:", my_list_2)
+    del my_list_1[0]  # Presta atención a esta línea.
+    print("Print #3:", my_list_1)
+    print("Print #4:", my_list_2)
+
+
+my_list_2 = [2, 3]
+my_function(my_list_2)
+print("Print #5:", my_list_2)
+
